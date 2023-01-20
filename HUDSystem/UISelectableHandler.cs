a203b6de0973e5f7ae33ad2097746125ff821b29
Alan Silva <alan.ferreirasilva15@gmail.com>
2023-01-20 18:47:11 -0300

    namespace update

--- HUDSystem/UISelectableHandler.cs
+++ HUDSystem/UISelectableHandler.cs
@@ -1,25 +1,27 @@
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-
-public class UISelectableHandler : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler
+namespace CoreSystem.HUDSystem
 {
-    private const float AnimationVelocity = 1;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.EventSystems;
 
-    [SerializeField] private Selectable _selectable;
-    [Range(0.1f, 0.75f)]
-    [SerializeField] private float _animationEffect = 0.75f;
+    public class UISelectableHandler : MonoBehaviour, IPointerDownHandler, IPointerExitHandler, IPointerUpHandler
+    {
+        private const float AnimationVelocity = 1;
 
-    private Vector2 _initialSize;
-    private Vector2 _downEffectSize;
+        [SerializeField] private Selectable _selectable;
+        [Range(0.1f, 0.75f)]
+        [SerializeField] private float _animationEffect = 0.75f;
 
-    private int _state;
-    private Vector2 _velocity;
+        private Vector2 _initialSize;
+        private Vector2 _downEffectSize;
 
-    private RectTransform _myRectTransform => (RectTransform)transform;
+        private int _state;
+        private Vector2 _velocity;
+
+        private RectTransform _myRectTransform => (RectTransform)transform;
 
 #if UNITY_EDITOR
-    #region EDITOR 
+        #region EDITOR 
     private void Reset() => GetResource();
 
     private void OnValidate() => GetResource();
@@ -32,62 +34,63 @@ public class UISelectableHandler : MonoBehaviour, IPointerDownHandler, IPointerE
             _selectable.transition = Selectable.Transition.None;
         }
     }
-    #endregion
+        #endregion
 #endif
 
-    private void Awake()
-    {
-        _initialSize = _myRectTransform.sizeDelta;
-        _downEffectSize = _initialSize * _animationEffect;
-        _velocity = Vector2.one * AnimationVelocity;
-    }
-
-    private void LateUpdate()
-    {
-        if(_state == 0)
+        private void Awake()
         {
-            if (_myRectTransform.sizeDelta != _initialSize)
-                UpAnimation();
+            _initialSize = _myRectTransform.sizeDelta;
+            _downEffectSize = _initialSize * _animationEffect;
+            _velocity = Vector2.one * AnimationVelocity;
         }
-        else
+
+        private void LateUpdate()
         {
-            if (_myRectTransform.sizeDelta != _downEffectSize)
-                DownAnimation();
+            if (_state == 0)
+            {
+                if (_myRectTransform.sizeDelta != _initialSize)
+                    UpAnimation();
+            }
+            else
+            {
+                if (_myRectTransform.sizeDelta != _downEffectSize)
+                    DownAnimation();
+            }
         }
-    }
 
-    private void DownAnimation()
-    {
-        _myRectTransform.sizeDelta = Vector2.SmoothDamp(_myRectTransform.sizeDelta, _downEffectSize, ref _velocity, Time.deltaTime);
-    }
-
-    private void UpAnimation()
-    {
-        _myRectTransform.sizeDelta = Vector2.SmoothDamp(_myRectTransform.sizeDelta, _initialSize, ref _velocity, Time.deltaTime);
-    }
-
-    #region OnPointer's Interface implementation
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (_selectable.interactable)
-            _state = 1;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (_state == 1)
+        private void DownAnimation()
         {
-            _state = 0;
+            _myRectTransform.sizeDelta = Vector2.SmoothDamp(_myRectTransform.sizeDelta, _downEffectSize, ref _velocity, Time.deltaTime);
         }
+
+        private void UpAnimation()
+        {
+            _myRectTransform.sizeDelta = Vector2.SmoothDamp(_myRectTransform.sizeDelta, _initialSize, ref _velocity, Time.deltaTime);
+        }
+
+        #region OnPointer's Interface implementation
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_selectable.interactable)
+                _state = 1;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (_state == 1)
+            {
+                _state = 0;
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_state == 1)
+                _state = 0;
+        }
+
+        #endregion
+
     }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if(_state == 1)
-            _state = 0;
-    }
-
-    #endregion
-
 }
--- ObservableSystem/ObservableProperty.cs
+++ ObservableSystem/ObservableProperty.cs
@@ -1,11 +1,14 @@
-using System;
-
-[Serializable]
-public class ObservableProperty<T>
+namespace CoreSystem.ObservableSystem
 {
-    private T _value;
-    public T Value { get { return _value; } set { _value = value; OnValueChangedWithReturn?.Invoke(value); OnValueChanged?.Invoke(); } }
+    using System;
 
-    public event Action<T> OnValueChangedWithReturn;
-    public event Action OnValueChanged;
+    [Serializable]
+    public class ObservableProperty<T>
+    {
+        private T _value;
+        public T Value { get { return _value; } set { _value = value; OnValueChangedWithReturn?.Invoke(value); OnValueChanged?.Invoke(); } }
+
+        public event Action<T> OnValueChangedWithReturn;
+        public event Action OnValueChanged;
+    }
 }
